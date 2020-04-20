f2f49deaca2e0f25dbfd4c62887a4061d1204ebb
Alexandru Buruiana <alexandru.buruiana8@gmail.com>
2020-04-20 13:19:41 +0300

    Update scss for signin page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -14,30 +14,28 @@ class SignIn extends React.Component {
         this.state = {
             email: '',
             password: ''
-        }
+        };
     }
 
     handleSubmit = event => {
         event.preventDefault();
 
-        this.ListeningStateChangedEvent({ email: '', password: '' })
-    }
+        this.setState({ email: '', password: '' });
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
 
-        this.setState({ [name]: value })
-    }
-
+        this.setState({ [name]: value });
+    };
 
     render() {
         return (
             <div className='sign-in'>
-                <h2> I already have an account</h2>
-                <span>Sign in with you email and password.</span>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-
                     <FormInput
                         name='email'
                         type='email'
@@ -46,29 +44,24 @@ class SignIn extends React.Component {
                         label='email'
                         required
                     />
-
                     <FormInput
                         name='password'
                         type='password'
-                        value={this.state.email}
+                        value={this.state.password}
                         handleChange={this.handleChange}
                         label='password'
                         required
                     />
-
-                    <CustomButton type='submit'>
-                        Sign in
+                    <div className='buttons'>
+                        <CustomButton type='submit'> Sign in </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign in with Google
                     </CustomButton>
-
-                    <CustomButton onClick={signInWithGoogle}>
-                        {' '}
-                        Sign in with Google {''}
-                    </CustomButton>
+                    </div>
                 </form>
             </div>
         );
     }
 }
-
 
 export default SignIn;
